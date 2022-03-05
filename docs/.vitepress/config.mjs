08fe1dc700dc5fbe11aab64f5375f186494b4e88
Piotr Kaminski <piotr@ideanest.com>
2022-03-05 00:12:26 -0800

    Fix logo.

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -15,7 +15,7 @@ export default defineConfig({
   },
 
   themeConfig: {
-    logo: 'logo.png',
+    logo: '/logo.png',
     repo: 'lastolivegames/becsy',
     docsDir: 'docs',
     docsBranch: 'main',
